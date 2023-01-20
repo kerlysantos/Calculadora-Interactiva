09cc1b470c53e6de3b54edb201d27357240a1adb
kerlysantos <santoskerly48@gmail.com>
2023-01-20 15:36:28 -0300

    calculadora-imperativa

---
+++ src/componentes/BotonClear.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../hojas-de-Estilos/BotonClear.css'
+
+const BotonClear = (props) => (
+    <div className="boton-clear" 
+    onClick={props.manejarClear}>
+    {props.children}
+    </div>
+);
+
+export default BotonClear;
